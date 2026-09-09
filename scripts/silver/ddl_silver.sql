690150446f9f0e35943ee5edc4195f404d04cd36
Zwe P <zwepy02@gmail.com>
2026-09-09 17:26:38 +0700

    DDL for Silver

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,21 @@
+-- DDL for Silver -- 
+-- Removed unnecessary columns
+
+DROP TABLE IF EXISTS silver.real_news;
+CREATE TABLE silver.real_news(
+	headline TEXT,
+	description TEXT,
+	authors TEXT,
+	published_date	DATE,
+	dwh_create_time TIMESTAMP DEFAULT clock_timestamp()
+);
+
+DROP TABLE IF EXISTS silver.fake_news;
+CREATE TABLE silver.fake_news(
+	headline TEXT,
+	description TEXT,
+	authors TEXT,
+	published_date DATE,
+	dwh_create_time TIMESTAMP DEFAULT clock_timestamp()
+);
+
